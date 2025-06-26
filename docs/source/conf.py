f647dc800a3f63f505b920295b923bc010191b6d
Michael Fischer <fischer@3sconsult.de>
2025-06-26 11:46:55 +0200

    start version

---
+++ docs/source/conf.py
@@ -0,0 +1,52 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'sir3stoolkit'
+copyright = '2025, misc'
+author = 'misc'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+autodoc_mock_imports = ["clr", "System", "Sir3S_Repository", "Sir3S_Toolkit"]
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+]
+
+# generate autosummary pages
+autosummary_generate = True
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,23 @@
+.. sir3stoolkit documentation master file, created by
+   sphinx-quickstart on Fri Jun 20 13:49:50 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to sir3stoolkit's documentation!
+========================================
+
+Test 123 for v15.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   references/modules 
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
