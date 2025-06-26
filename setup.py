344f7ed6ad1c21dc62fa415cf28f53467e3f395a
Michael Fischer <fischer@3sconsult.de>
2025-06-26 12:05:36 +0200

    .

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sir3stoolkit',
-    version='0.0.0',
+    version='0.0.1',
     description='SIR3S Toolkit',
     long_description='SIR3S Python Toolkit',
     author="Nischal Giriyan",
